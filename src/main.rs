78e5305dbc48f7ca5fbaa67ab2625b6908b2e12b
furuhama <furuhama@users.noreply.github.com>
2018-05-03 21:40:10 +0900

    Define Peano enum & Add test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// Peano's national number
+
+enum Peano {
+    Zero,
+    Suc,
+}
+
+fn zero() -> Peano {
+    Peano::Zero
+}
+
+#[test]
+fn test_zero() {
+    assert!(match zero() {
+        Peano::Zero => true,
+        _ => false
+    });
+}
+
 fn main() {
     println!("Hello, world!");
 }
